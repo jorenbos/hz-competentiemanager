8282567cd3b9c581c4c11dc62189bb7db83d7d8d
Jesse Kramer <kram0021@hz.nl>
2016-11-21 14:53:30 +0100

    test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -75,7 +75,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Smakelijk Project
+                    Smakelijk Project - Voor de fijnproevers
                 </div>
 
                 <div class="links">
