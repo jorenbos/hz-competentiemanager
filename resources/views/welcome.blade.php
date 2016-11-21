91363943a11548ab2fa2ae7749cdd13d67b8e9ed
Roel van Endhoven <roel@nfnty.nl>
2016-11-21 14:09:31 +0100

    Heroku Procfile

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -75,7 +75,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Smakelijk Project
                 </div>
 
                 <div class="links">
