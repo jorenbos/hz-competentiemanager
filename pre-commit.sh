dba06812f4550b696b1a5977a3f421f6727e02a2
Jesse Kramer <kram0021@hz.nl>
2016-11-28 22:04:43 +0100

    fixed a typo

--- pre-commit.sh
+++ pre-commit.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 echo "[pre-commit] Running tests.."
-exec vendor/bin/phpcbf ./app ./tests
-exec vendor/bin/phpmd 'app,tests' text 'phpmd-ruleset.xml'
+vendor/bin/phpcbf ./app ./tests
+vendor/bin/phpmd 'app,tests' text 'phpmd-ruleset.xml'
 
 echo "[pre-commit] All done. Happy coding!"
