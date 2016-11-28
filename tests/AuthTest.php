3a422a544002f7977256f9c2c5ada948e91dedee
Jesse Kramer <kram0021@hz.nl>
2016-11-28 11:17:28 +0100

    fixed tests

--- tests/AuthTest.php
+++ tests/AuthTest.php
@@ -3,32 +3,45 @@
 use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
+use App\User;
 
 class AuthTest extends TestCase
 {
-    public function testRegister()
-    {
-      $this->visit('/register')
-           ->type('John Doe', 'name')
-           ->type('johndoe@example.com', 'email')
-           ->type('admin123', 'password')
-           ->type('admin123', 'password_confirmation')
-           ->press('Register')
-           ->seePageIs('/dashboard');
-    }
 
-    public function testLogout()
-    {
-      $this->visit('/logout')
-           ->seePageIs('/');
-    }
+  use DatabaseMigrations;
 
-    public function testLogin()
-    {
-      $this->visit('/login')
-           ->type('johndoe@example.com', 'email')
-           ->type('admin123', 'password')
-           ->press('Sign in')
-           ->seePageIs('/dashboard');
-    }
+  public function testRegister()
+  {
+    $this->visit('/register')
+         ->type('John Doe', 'name')
+         ->type('johndoe@example.com', 'email')
+         ->type('admin123', 'password')
+         ->type('admin123', 'password_confirmation')
+         ->press('Register')
+         ->seePageIs('/dashboard');
+  }
+
+  public function testLogout()
+  {
+    $user = factory(App\User::class)->create();
+
+    $this->actingAs($user)
+         ->visit('/logout')
+         ->seePageIs('/');
+  }
+
+  public function testLogin()
+  {
+    $user = User::create([
+      'name' => 'John Doe',
+      'email' => 'johndoe@example.com',
+      'password' => bcrypt('admin123')
+    ]);
+
+    $this->visit('/login')
+         ->type('johndoe@example.com', 'email')
+         ->type('admin123', 'password')
+         ->press('Sign in')
+         ->seePageIs('/dashboard');
+  }
 }
