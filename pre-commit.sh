94844a5d7b0a950cba4dd9f873b5c1d49a06790b
Jesse Kramer <kram0021@hz.nl>
2016-11-28 22:33:49 +0100

    commented out the phplint line, because its not configured enough yet

--- pre-commit.sh
+++ pre-commit.sh
@@ -2,6 +2,6 @@
 
 echo "[pre-commit] Running tests.."
 vendor/bin/phpcbf ./app ./tests
-vendor/bin/phpmd 'app,tests' text 'phpmd-ruleset.xml'
+vendor/bin/phpmd 'app,tests,!app/console,!app/exceptions,!app/providers' text 'phpmd-ruleset.xml'
 
 echo "[pre-commit] All done. Happy coding!"
