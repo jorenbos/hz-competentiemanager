e7ec42e1bd726ea689c4620829706fa113afb6c4
quintijn vos <vos0043@bitbucket.org>
2016-11-28 14:46:16 +0100

    migrations added

--- database/migrations/2016_11_28_130053_create_project_table.php
+++ database/migrations/2016_11_28_130053_create_project_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateCompetencyModel extends Migration
+class CreateProjectTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,12 @@ class CreateCompetencyModel extends Migration
      */
     public function up()
     {
-        //
+      Schema::create('projects', function (Blueprint $table) {
+          $table->increments('id');
+          $table->string('name');
+          $table->string('projectnumber');
+          $table->timestamps();
+      });
     }
 
     /**
@@ -23,6 +28,6 @@ class CreateCompetencyModel extends Migration
      */
     public function down()
     {
-        //
+        Schema::drop('projects');
     }
 }
