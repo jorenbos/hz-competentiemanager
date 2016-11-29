a7ec56d01f61f18ff33de9a8fc74a1a9a64d6ac4
Jesse Kramer <kram0021@hz.nl>
2016-11-29 12:32:10 +0100

    fixed composer dependencies

--- pre-commit.sh
+++ pre-commit.sh
@@ -2,6 +2,6 @@
 
 echo "[pre-commit] Running tests.."
 vendor/bin/phpcbf ./app ./tests
-vendor/bin/phpmd app,tests xml codesize,unusedcode,naming --exclude app/console,app/exceptions,app/providers
+vendor/bin/phpmd app,tests text codesize,unusedcode,naming --exclude app/console,app/exceptions,app/providers
 
 echo "[pre-commit] All done. Happy coding!"
