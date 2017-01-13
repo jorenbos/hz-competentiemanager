0ea807adcd38c1da55341547d36cfed1a5181998
Gert <gertp93@gmail.com>
2017-01-13 16:44:16 +0100

    updated pre-comit.sh, wont affet anything since its commented out

--- pre-commit.sh
+++ pre-commit.sh
@@ -2,6 +2,7 @@
 
 echo "[pre-commit] Running tests.."
 vendor/bin/phpcbf ./app ./tests
-# vendor/bin/phpmd app,tests text codesize,unusedcode,naming --exclude app/console,app/exceptions,app/providers
+#This doesn't work on windows for some reason, run these tests from your vagrant box
+#vendor/bin/phpmd app,tests text codesize,unusedcode,naming --exclude app/Console/*,app/Exceptions/*,app/Providers/*
 
 echo "[pre-commit] All done. Happy coding!"
