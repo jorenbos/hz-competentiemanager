d8f93a09f87d4c62146842d7be38d91712a1cce7
Jesse Kramer <kram0021@hz.nl>
2017-01-13 12:20:42 +0100

    added a couple of repository tests to competency

--- tests/CompetencyTest.php
+++ tests/CompetencyTest.php
@@ -3,10 +3,42 @@
 use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
+use App\Repositories\CompetencyRepository;
 
 class CompetencyTest extends TestCase
 {
     use DatabaseMigrations;
+
+    public function testRepositoryGetById()
+    {
+        $competencyRepository = new CompetencyRepository();
+        $competency = factory(App\Models\Competency::class)->create();
+        $this->assertEquals($competency->id, $competencyRepository->getById($competency->id)->id);
+    }
+
+    public function testRepositoryGetAll()
+    {
+        $competencyRepository = new CompetencyRepository();
+        factory(App\Models\Competency::class, 10)->create();
+        $this->assertEquals(10, count($competencyRepository->getAll()));
+    }
+
+    public function testRepositoryCreate()
+    {
+        $competencyRepository = new CompetencyRepository();
+        $comp = $competencyRepository->create([
+            'name' => 'Memes Posten 1a', 
+            'abbreviation' => 'MEME', 
+            'description' => 'blablabla', 
+            'ec_value' => 5.0, 
+            'cu_code' => 'CU123456'
+        ]);
+        $this->assertEquals('Memes Posten 1a', $comp->name);
+        $this->assertEquals('MEME', $comp->abbreviation);
+        $this->assertEquals('blablabla', $comp->description);
+        $this->assertEquals(5.0, $comp->ec_value);
+        $this->assertEquals('CU123456', $comp->cu_code);   
+    }
 
     public function testRelationWithProjects()
     {
