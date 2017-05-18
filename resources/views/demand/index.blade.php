9d2cbb965b24086d497dd1aec3f9c84200c166fd
Gert de Pagter <gertp93@gmail.com>
2017-05-18 21:16:35 +0200

    removed max potentieel vraag

--- resources/views/demand/index.blade.php
+++ resources/views/demand/index.blade.php
@@ -15,7 +15,6 @@
     <table class="table">
         <thead>
             <th>Competentie</th>
-            <th>Maximum potentiele vraag</th>
             <th>Gewogen gemiddelde vraag</th>
         </thead>
 
@@ -23,7 +22,6 @@
             @foreach ($competencies as $competency)
                 <tr>
                     <td>{{$competency['competency']->name}}</td>
-                    <td>{{$competency['count']}}</td>
                     <td>{{$competency['mean_demand']}}</td>
 
                 </tr>
